2846d8520f0c240d02e70f216cb75216a0c9f5da
Ally Okoba <allyagutu@gmail.com>
2025-05-25 07:33:07 -0700

    0-javascript_is_amazing.js

---
+++ 0-javascript_is_amazing.js
@@ -0,0 +1,9 @@
+// This script defines a constant variable and prints its value to the console.
+
+// Create a constant variable named 'myVar' with the specified string value.
+// The 'const' keyword ensures that the variable cannot be reassigned after its initial declaration.
+const myVar = "JavaScript is amazing";
+
+// Use console.log() to print the value of 'myVar' to the standard output.
+// This will display "JavaScript is amazing" in the console when the script is executed.
+console.log(myVar);
