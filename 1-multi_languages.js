be066e4bf306666d73a06fcee71e92cbc56e46ee
Ally Agutu <allyagutu@gmail.com>
2025-05-25 08:35:27 -0700

    1-multi_languages.js

---
+++ 1-multi_languages.js
@@ -0,0 +1,11 @@
+// This script prints three different lines to the console.
+
+// Print the first line: "C is fun"
+console.log("C is fun");
+
+// Print the second line: "Python is cool"
+console.log("Python is cool");
+
+// Print the third line: "JavaScript is amazing"
+// This line directly prints the specified string.
+console.log("JavaScript is amazing");
